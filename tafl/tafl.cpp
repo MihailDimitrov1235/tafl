3c427304fad43f9782411f23776d8036c337932a
Mihail <107298750+MihailDimitrov1235@users.noreply.github.com>
2024-12-21 19:19:32 +0200

    added the game start menu

--- tafl/tafl.cpp
+++ tafl/tafl.cpp
@@ -13,11 +13,30 @@
 * <main program file>
 *
 */
-
-
 #include <iostream>
 using namespace std;
 
-int main() {
+void startGame() {
+	cout << "Welcome to Tafl\n\n";
+	cout << "1) Start game\n";
+	cout << "2) Quit\n";
+	cout << "Choose one of the options (1 or 2) :";
 
+	char option;
+	cin >> option;
+
+	while (option != '1' && option != '2') {
+		cout << "Invalid option. Type 1 or 2 :";
+		cin >> option;
+	}
+
+	if (option == '2')
+	{
+		return;
+	}
+}
+
+
+int main() {
+	startGame();
 }
