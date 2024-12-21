87eb23bab5e95cf12b7af235f371d13dab0ee2d9
Mihail <107298750+MihailDimitrov1235@users.noreply.github.com>
2024-12-21 13:42:55 +0200

    added project information to the main file

--- tafl/tafl.cpp
+++ tafl/tafl.cpp
@@ -1,7 +1,23 @@
+
+/**
+*
+* Solution to course project # 7
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2023/2024
+*
+* @author Mihail Dimitrov
+* @idnumber 3MI0600472
+* @compiler VC
+*
+* <main program file>
+*
+*/
+
+
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+
 }
