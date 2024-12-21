57e6cdac8620bbd9d733d9104b94363bbd2f3a7e
Mihail <107298750+MihailDimitrov1235@users.noreply.github.com>
2024-12-21 19:45:02 +0200

    added a board size select menu

--- tafl/tafl.cpp
+++ tafl/tafl.cpp
@@ -17,26 +17,55 @@
 using namespace std;
 
 void startGame() {
-	cout << "Welcome to Tafl\n\n";
-	cout << "1) Start game\n";
-	cout << "2) Quit\n";
-	cout << "Choose one of the options (1 or 2) :";
+	cout << endl << endl << "Choose game board size:\n\n";
+	cout << "1) 13x13\n";
+	cout << "2) 11x11 (Standard)\n";
+	cout << "3) 9x9\n";
+	cout << "4) 7x7\n\n";
+	cout << "Choose one of the options (1 - 4) : ";
 
-	char option;
+	size_t option;
 	cin >> option;
 
-	while (option != '1' && option != '2') {
-		cout << "Invalid option. Type 1 or 2 :";
+	while (option < 1 || option > 4) {
+		cout << "Invalid option. Type a number between 1 and 4 : ";
 		cin >> option;
 	}
 
-	if (option == '2')
+	size_t boardSize = 11;
+	switch (option) {
+	case 1: boardSize = 13; break;
+	case 2: boardSize = 11; break;
+	case 3: boardSize = 9; break;
+	case 4: boardSize = 7; break;
+	}
+	cout << boardSize;
+}
+
+void mainMenu() {
+	cout << "Welcome to Tafl\n\n\n";
+	cout << "Main Menu\n\n";
+	cout << "1) Start game\n";
+	cout << "2) Quit\n\n";
+	cout << "Choose one of the options (1 or 2) : ";
+
+	int option;
+	cin >> option;
+
+	while (option < 1 || option > 2) {
+		cout << "Invalid option. Type 1 or 2 : ";
+		cin >> option;
+	}
+
+	if (option == 2)
 	{
 		return;
 	}
+
+	startGame();
 }
 
 
 int main() {
-	startGame();
+	mainMenu();
 }
