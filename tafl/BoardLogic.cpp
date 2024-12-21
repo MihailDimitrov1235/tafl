4bece9956ebb0293540de0916f052fb624e96acf
Mihail <107298750+MihailDimitrov1235@users.noreply.github.com>
2024-12-21 20:44:03 +0200

    added project information to the board logic file

--- tafl/BoardLogic.cpp
+++ tafl/BoardLogic.cpp
@@ -1,3 +1,18 @@
+/**
+*
+* Solution to course project # 7
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2023/2024
+*
+* @author Mihail Dimitrov
+* @idnumber 3MI0600472
+* @compiler VC
+*
+* <board logic file>
+*
+*/
+
 #include <iostream>
 #include "BoardLogic.h"
 using namespace std;
