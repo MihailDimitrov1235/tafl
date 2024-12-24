86451f92d32aaa216995e5a4b6f356d41281c357
Mihail <107298750+MihailDimitrov1235@users.noreply.github.com>
2024-12-24 22:00:39 +0200

    added a 13x13 board

--- tafl/BoardLogic.cpp
+++ tafl/BoardLogic.cpp
@@ -24,6 +24,22 @@ const char DEFENDER = 'D';
 const char EDGE = '*';
 const char CASTLE = 28;
 
+const char parlettBoard[13][13] = {
+	{ EDGE, EMPTY, EMPTY, EMPTY,  ATTACKER, ATTACKER, ATTACKER, ATTACKER, ATTACKER, EMPTY, EMPTY, EMPTY,  EDGE },
+	{ EMPTY, EMPTY, EMPTY, EMPTY,  EMPTY, ATTACKER, EMPTY, ATTACKER, EMPTY, EMPTY, EMPTY, EMPTY,  EMPTY },
+	{ EMPTY, EMPTY, EMPTY, EMPTY,  EMPTY, EMPTY, ATTACKER, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,  EMPTY },
+	{ EMPTY, EMPTY, EMPTY, EMPTY,  EMPTY, EMPTY, DEFENDER, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,  EMPTY },
+	{ ATTACKER, EMPTY, EMPTY, EMPTY,  DEFENDER, EMPTY, EMPTY, EMPTY, DEFENDER, EMPTY, EMPTY, EMPTY,  ATTACKER },
+	{ ATTACKER, ATTACKER, EMPTY, EMPTY,  EMPTY, DEFENDER, DEFENDER, DEFENDER, EMPTY, EMPTY, EMPTY, ATTACKER,  ATTACKER },
+	{ ATTACKER, EMPTY, ATTACKER, DEFENDER,  EMPTY, DEFENDER, KING, DEFENDER, EMPTY, DEFENDER, ATTACKER, EMPTY,  ATTACKER },
+	{ ATTACKER, ATTACKER, EMPTY, EMPTY,  EMPTY, DEFENDER, DEFENDER, DEFENDER, EMPTY, EMPTY, EMPTY, ATTACKER,  ATTACKER },
+	{ ATTACKER, EMPTY, EMPTY, EMPTY,  DEFENDER, EMPTY, EMPTY, EMPTY, DEFENDER, EMPTY, EMPTY, EMPTY,  ATTACKER },
+	{ EMPTY, EMPTY, EMPTY, EMPTY,  EMPTY, EMPTY, DEFENDER, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,  EMPTY },
+	{ EMPTY, EMPTY, EMPTY,EMPTY,  EMPTY, EMPTY, ATTACKER, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,  EMPTY },
+	{ EMPTY, EMPTY, EMPTY,EMPTY,  EMPTY, ATTACKER, EMPTY, ATTACKER, EMPTY, EMPTY, EMPTY, EMPTY,  EMPTY },
+	{ EDGE, EMPTY, EMPTY,EMPTY,  ATTACKER, ATTACKER, ATTACKER, ATTACKER, ATTACKER, EMPTY, EMPTY, EMPTY,  EDGE },
+};
+
 const char hnefataflBoard[11][11] = {
 	{ EDGE, EMPTY, EMPTY, ATTACKER, ATTACKER, ATTACKER, ATTACKER, ATTACKER, EMPTY, EMPTY, EDGE },
 	{ EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, ATTACKER, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY },
@@ -64,14 +80,15 @@ size_t getBoardSize() {
 	cout << endl << endl << "Choose game board size:\n\n";
 	cout << "1) 11x11 (Standard)\n";
 	cout << "2) 9x9\n";
-	cout << "3) 7x7\n\n";
-	cout << "Choose one of the options (1 - 3) : ";
+	cout << "3) 7x7\n";
+	cout << "4) 13x13\n\n";
+	cout << "Choose one of the options (1 - 4) : ";
 
 	size_t option;
 	cin >> option;
 
-	while (option < 1 || option > 3) {
-		cout << "Invalid option. Type a number between 1 and 3 : ";
+	while (option < 1 || option > 4) {
+		cout << "Invalid option. Type a number between 1 and 4 : ";
 		cin >> option;
 	}
 
@@ -80,6 +97,7 @@ size_t getBoardSize() {
 	case 1: boardSize = 11; break;
 	case 2: boardSize = 9; break;
 	case 3: boardSize = 7; break;
+	case 4: boardSize = 13; break;
 	}
 
 	return boardSize;
@@ -130,6 +148,21 @@ char** getBoardOfSize7() {
 	return board;
 }
 
+char** getBoardOfSize13() {
+	const size_t boardSize = 13;
+	char** board = new char* [boardSize];
+	for (size_t i = 0; i < boardSize; i++)
+	{
+		char* row = new char[boardSize];
+		for (size_t j = 0; j < boardSize; j++)
+		{
+			row[j] = parlettBoard[i][j];
+		}
+		board[i] = row;
+	}
+	return board;
+}
+
 char** getBoard(size_t boardSize) {
 	switch (boardSize) {
 	case 11:
@@ -138,6 +171,8 @@ char** getBoard(size_t boardSize) {
 		return getBoardOfSize9();
 	case 7:
 		return getBoardOfSize7();
+	case 13:
+		return getBoardOfSize13();
 	default:
 		cout << "Invalid boardSize!!!\n";
 		return nullptr;
